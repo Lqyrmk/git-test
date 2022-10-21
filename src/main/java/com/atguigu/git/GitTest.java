1f8a90845087c632da5a7a549cb47b1dbdf7f68c
Lqyrmk <1638574981@qq.com>
2022-10-22 02:43:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Description
+ * @Author YuanmingLiu
+ * @Date 2022/10/22 2:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
